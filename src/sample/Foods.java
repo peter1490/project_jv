45d8640b210e5592bc6be89fa21e8935f0464955
peter1490 <peterbalivet@gmail.com>
2019-08-15 03:53:01 +0200

    Login + User + Recipes + Foods =  Done

---
+++ src/sample/Foods.java
@@ -0,0 +1,33 @@
+package sample;
+
+public class Foods {
+    private int id;
+    private String name;
+    private int number;
+
+    public Foods(){}
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+}
